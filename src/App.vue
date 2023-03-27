38ed58a0dfd0778f992cb7d61e1e6cd44ed67961
rodrigo laime <rodrigolaime141@gmail.com>
2023-03-27 17:33:45 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <button @click="show = !show">Menu</button>
+  <transition name="fade">
+    <Menu v-show="show" />
+  </transition>
+
+  <div>
+    <Modal />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Menu from "./components/Menu.vue";
+import Modal from "./components/Modal.vue";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { Menu, Modal },
+  data() {
+    return {
+      show: false,
+    };
   },
 };
 </script>
-
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button @click="toggle">Modal</button>
+    <div v-show="show" class="modal">
+      <h1>Titulo</h1>
+      <p>Mensage</p>
+      <button @click="toggle">Cerrar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    toggle() {
+      this.show = !this.show;
+    },
+  },
+};
+</script>
